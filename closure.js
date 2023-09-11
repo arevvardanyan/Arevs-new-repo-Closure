f186876531d26c9d562a23d6735058a18c27b128
Arev <vardanyanarevik23@gmail.com>
2023-09-11 12:25:06 +0400

    ex-4 to get and set person name and age

--- closure.js
+++ closure.js
@@ -102,3 +102,41 @@ const cachedCalculation = createCachingFunction();
 // console.log(cachedCalculation(5)); // Output: 10 (calculated)
 // console.log(cachedCalculation(6));
 // console.log(cachedCalculation(5));
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 4
+
+const createPerson = function(name,age){
+    let nameSet = name;
+    let ageSet = age
+    const getName = function(){
+        return nameSet
+    }
+    const getAge = function(){
+        return ageSet
+    }
+    const setName = function(name){
+        nameSet = name
+        return nameSet
+    }
+    const setAge = function(age){
+        ageSet = age
+        return ageSet
+    }
+
+    return {
+        getName,
+        getAge,
+        setName,
+        setAge
+    }
+}
+
+// const person = createPerson("Alice", 30); 
+// console.log(person.getName()); // Output: "Alice"
+// console.log(person.getAge()); // Output: 30 
+// person.setName("Bob"); 
+// person.setAge(25); 
+// console.log(person.getName()); // Output: "Bob"
+// console.log(person.getAge());
